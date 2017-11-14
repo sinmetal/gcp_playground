6bc308a5d6263677037d3b490efcaed4424e40d7
sinmetal <metal.tie@gmail.com>
2017-11-14 09:41:02 +0900

    Datastore Putを追加

---
+++ datastore/datastore.go
@@ -0,0 +1,20 @@
+package datastore
+
+import (
+	"golang.org/x/net/context"
+
+	"cloud.google.com/go/datastore"
+)
+
+type Row struct {
+	Value string `json:"value"`
+}
+
+func Put(ctx context.Context, client *datastore.Client, row Row) (*datastore.Key, error) {
+	key, err := client.Put(ctx, datastore.IncompleteKey("Item", nil), &row)
+	if err != nil {
+		return nil, err
+	}
+
+	return key, nil
+}
